a1536cb4b5c89a5d5761bde3d4aa406ff13c10ba
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2024-11-16 20:01:43 +0530

    Update DAY1_P1.java

--- DAY1_P1.java
+++ DAY1_P1.java
@@ -1,25 +1,4 @@
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            DAY1_P1 ob = new DAY1_P1();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-class DAY1_P1 {
+class Solution {
     public int getSecondLargest(int[] arr) {
         if (arr == null || arr.length < 2) {
             return -1; // If array is null or has less than 2 elements
@@ -40,4 +19,3 @@ class DAY1_P1 {
         return secondLargest == Integer.MIN_VALUE ? -1 : secondLargest;
     }
 }
-
