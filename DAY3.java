2b33d7c4aae00cb4fc5fafe995ee6e77f7df9dfe
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2024-11-17 11:20:03 +0530

    Create DAY3.java

---
+++ DAY3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static void reverseArray(int[] arr) {
+        int start = 0;            // Pointer to the beginning of the array
+        int end = arr.length - 1; // Pointer to the end of the array
+
+        // Loop until start crosses or meets end
+        while (start < end) {
+            // Swap elements at start and end
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            // Move the pointers inward
+            start++;
+            end--;
+        }
+    }
+}
