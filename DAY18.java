71d72091ee7c8798aa2d17ef17dba841d71c00a7
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-01-19 22:53:04 +0530

    Create DAY18.java

---
+++ DAY18.java
@@ -0,0 +1,59 @@
+//Module 2 - Strings
+//Problem 5 - Day18 - Search Pattern (KMP-Algorithm)
+
+class Solution {
+
+    ArrayList<Integer> search(String pat, String txt) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int m = pat.length();
+        int n = txt.length();
+
+        // Step 1: Build the LPS array
+        int[] lps = buildLPSArray(pat);
+
+        // Step 2: Use LPS to search for the pattern in the text
+        int i = 0; // Index for txt
+        int j = 0; // Index for pat
+        while (i < n) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == m) {
+                // Pattern found, add starting index to result
+                result.add(i - j);
+                j = lps[j - 1];
+            } else if (i < n && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int[] buildLPSArray(String pat) {
+        int m = pat.length();
+        int[] lps = new int[m];
+        int len = 0; // Length of the previous longest prefix suffix
+        int i = 1;
+
+        while (i < m) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
