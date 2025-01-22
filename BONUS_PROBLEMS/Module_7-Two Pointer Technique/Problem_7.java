8f9b1a36ab276412a513f2667ec44fc72dcd12a1
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-01-22 19:13:52 +0530

    Create Problem_7.java

---
+++ BONUS_PROBLEMS/Module_7-Two Pointer Technique/Problem_7.java
@@ -0,0 +1,31 @@
+//Problem - Pair Sum in a Sorted and Rotated Array
+
+class Solution {
+    static boolean pairInSortedRotated(int arr[], int target) {
+        // Your code here
+        int n = arr.length;
+        int i;
+        
+        for(i = 0; i < n - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                break;
+            }
+        }
+        
+        int l = (i + 1) % n;
+        
+        int r = i;
+        
+        while( l != r){
+            if (arr[l] + arr[r] == target) return true;
+            
+            if (arr[l] + arr[r] < target) l = (l + 1) % n;
+            
+            else
+                r = (n + r - 1) % n;
+        }
+    
+    
+    return false;
+    }
+}
