8c74cd8170341e11b44227206cacce970e95d869
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-01-22 16:30:25 +0530

    Create Problem_5.java

---
+++ BONUS_PROBLEMS/Module_2-String/Problem_5.java
@@ -0,0 +1,23 @@
+//Problem - Longest Prefix Suffix
+
+class Solution:
+    def longestPrefixSuffix(self, s):
+        n = len(s)
+        lps = [0] * n  # LPS array
+        length = 0  # Length of the previous longest prefix suffix
+        i = 1  # Start from the second character
+
+        while i < n:
+            if s[i] == s[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]  # Go back to the previous possible prefix
+                else:
+                    lps[i] = 0
+                    i += 1
+
+        return lps[-1]  # The last value in the LPS array
+
