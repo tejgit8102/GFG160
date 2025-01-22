492b99ab2160d4badcde35c73aae5599f2f55ec8
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-01-22 15:39:52 +0530

    Create DAY66.py

---
+++ DAY66.py
@@ -0,0 +1,48 @@
+#Module 9 - Linked List
+#Problem 2 - Day66 - Rotate a Linked List
+
+  # Your task is to complete this function
+
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+
+
+class Solution:
+    
+    #Function to rotate a linked list.
+    def rotate(self, head, k):
+        # code here
+        if not head or k == 0:
+            return head
+        
+        curr = head
+        length = 1
+        
+        while curr.next:
+            curr = curr.next
+            length += 1
+            
+        k %= length
+        
+        if k == 0:
+            return head
+        
+        curr.next = head
+        
+        
+        curr = head
+        for _ in range(1, k):
+            curr = curr.next
+        
+        newHead = curr.next
+        
+        curr.next = None
+        return newHead
+
+
