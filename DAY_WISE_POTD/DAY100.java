242f2866e9d9caaca473d8aa26dce6f8701428b0
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-02-22 10:31:35 +0530

    Create DAY100.java

---
+++ DAY_WISE_POTD/DAY100.java
@@ -0,0 +1,26 @@
+//Module 13 - Stack
+//Problem 2 - Day100 - Longest valid Parentheses
+
+class Solution {
+    static int maxLength(String s) {
+        // code here
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLength = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                
+                if(stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+        return maxLength;
+    }
+}
