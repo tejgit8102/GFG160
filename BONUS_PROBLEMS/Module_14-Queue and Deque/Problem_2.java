95f887795e339ac3b7901b8a44fd00ecd2e239f5
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-03-04 21:15:31 +0530

    Create Problem_2.java

---
+++ BONUS_PROBLEMS/Module_14-Queue and Deque/Problem_2.java
@@ -0,0 +1,32 @@
+//Problem - Maximum score from at most K jumps
+
+
+class Solution {
+    public int getScore(int arr[], int k) {
+        // code here
+        int n = arr.length;
+        
+        int[] dp = new int[n];
+        
+        dp[n - 1] = arr[n - 1];
+        
+        Deque<Integer> dq = new LinkedList<>();
+        dq.offer(n - 1);
+        
+        for(int i = n - 2; i >= 0; i--) {
+            while(!dq.isEmpty() && dq.peekFirst() > i + k) {
+                dq.pollFirst();
+            }
+            
+            dp[i] = arr[i] + dp[dq.peekFirst()];
+            
+            while(!dq.isEmpty() && dp[i] >= dp[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            
+            dq.offerLast(i);
+        }
+        
+        return dp[0];
+    }
+}
