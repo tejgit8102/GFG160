40ffbcf21454cd5eaa3faf4bee18416f0dcff19b
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-03-13 20:14:06 +0530

    Create DAY116.java

---
+++ DAY_WISE_POTD/DAY116.java
@@ -0,0 +1,32 @@
+//Module 15 - Dynamic Programming
+//Problem 7 - Day116 - Edit Distance
+
+class Solution {
+    public int editDistance(String s1, String s2) {
+        // Code here
+        int m = s1.length();
+        int n = s2.length();
+        
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0) {
+                    dp[i][j] = j;
+                }
+                else if(j == 0) {
+                    dp[i][j] = i;
+                }
+                else if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j],
+                                   Math.min(dp[i][j - 1],
+                                            dp[i - 1][j - 1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
