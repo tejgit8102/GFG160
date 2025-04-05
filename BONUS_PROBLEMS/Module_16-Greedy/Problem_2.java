0c2e3993eb2bb33418244d7685c62234dfbed9a7
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-04-05 08:27:46 +0530

    Create Problem_2.java

---
+++ BONUS_PROBLEMS/Module_16-Greedy/Problem_2.java
@@ -0,0 +1,37 @@
+//Problem - Police and Thieves
+
+class Solution {
+    public int catchThieves(char[] arr, int k) {
+        // code here
+        int n = arr.length;
+        
+        int i = 0, j = 0;
+        int count = 0;
+        
+        while(i < n && j < n) {
+            
+            while(i < n && arr[i] != 'P') i++;
+            
+            while(j < n && arr[j] != 'T') j++;
+            
+            if(i < n && j < n && Math.abs(i - j) <= k) {
+                count++;
+                
+                i++;
+                
+                j++;
+            }
+            
+            else if( j < n && j < i) {
+                j++;
+            }
+            
+            else if( i < n && i < j) {
+                i++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
