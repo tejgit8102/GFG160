25c6dbf768154fd39dde12e9871b5e4cc03d6e8b
Tejashwini S <166322007+tejgit8102@users.noreply.github.com>
2025-04-22 18:09:39 +0530

    Create DAY159.java

---
+++ DAY_WISE_POTD/DAY159.java
@@ -0,0 +1,14 @@
+//Module 19 - Bit Manipulation
+//Problem 1 - Day159 - Unique Number I
+
+
+class Solution {
+    public int findUnique(int[] arr) {
+        // code here
+        int res = 0;
+        for(int i = 0; i < arr.length; i++) {
+            res ^= arr[i];
+        }
+        return res;
+    }
+}
